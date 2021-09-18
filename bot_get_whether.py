a07f33265a4021b69906a0891824674a1ba51dda
Serega575719 <90559709+Serega575719@users.noreply.github.com>
2021-09-19 02:19:57 +0300

    Update bot_get_whether.py

--- bot_get_whether.py
+++ bot_get_whether.py
@@ -2,6 +2,8 @@ import telebot
 from telebot import types
 import time
 import whether
+from pyowm import OWM
+from pyowm.commons.enums import SubscriptionTypeEnum
 
 
 API_TOKEN = '1935823217:AAEdJYzUGwP465TdzhycGQn41wopqBV5A_o'
@@ -9,6 +11,49 @@ API_TOKEN = '1935823217:AAEdJYzUGwP465TdzhycGQn41wopqBV5A_o'
 bot = telebot.TeleBot(API_TOKEN)
 info = bot.get_me()
 
+def config():
+    config = {
+        'subscription_type': SubscriptionTypeEnum.FREE,
+        'language': 'ru',
+        'connection': {
+            'use_ssl': True,
+            'verify_ssl_certs': True,
+            'use_proxy': False,
+            'timeout_secs': 5
+        },
+        'proxies': {
+            'http': 'http://user:pass@host:port',
+            'https': 'socks5://user:pass@host:port'
+        }
+    }
+    return config
+
+API_TOKEN = '1180b504dd3815f626bc826139b04592'
+owm = OWM(API_TOKEN, config = config())
+
+whettrrt = {}
+
+
+class Whether:
+
+    @staticmethod
+    def get_whther_from_name_sity(sity):
+        try:
+            Weather_man = owm.weather_manager()
+            monitoring = Weather_man.weather_at_place(sity)
+            weather = monitoring.weather
+            status = weather.detailed_status
+            return f"В {sity} сейчас {status}, температура воздуха = {(weather.temp['temp'] - 273):.0f}"
+        except Exception:
+            return None
+
+    @staticmethod
+    def get_whther_from_geo_data(lat, lon):
+        get_Weather = owm.weather_manager().weather_at_coords(lat, lon)
+        place = get_Weather.location.name
+        status = get_Weather.weather.detailed_status
+        temp = get_Weather.weather.temp
+        return f"В {place} сейчас {status}, температура воздуха = {(temp['temp'] - 273):.0f}"
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['start'])
@@ -32,24 +77,24 @@ def get_txt(message):
     elif message.text == 'Погода':
         bot.reply_to(message, f"Отправте свои геоданные, если хотите узнать погоду в другом городе, отправте название "
                               f"города")
-        whether.whettrrt[message.chat.id] = True
-        print(f'{message.from_user.first_name} {message.from_user.last_name} получил запрос на погоду, {whether.whettrrt[message.chat.id]}')
+        whettrrt[message.chat.id] = True
     elif message.content_type == 'text':
-        wet_sity = whether.Whether.get_whther_from_name_sity(message.text)
-        if wet_sity is not None and whether.whettrrt[message.chat.id] == True:
+        wet_sity = Whether.get_whther_from_name_sity(message.text)
+        if wet_sity is not None and whettrrt[message.chat.id] == True:
             bot.send_message(message.chat.id, wet_sity)
-            whether.whettrrt[message.chat.id] = False
-            print(f'{message.from_user.first_name} {message.from_user.last_name} ввел город {whether.whettrrt[message.chat.id]}')
+            whettrrt[message.chat.id] = False
         elif wet_sity is not None:
             bot.send_message(message.chat.id, f"Повторите команду запроса погоды")
-        elif whether.whettrrt[message.chat.id] == True:
+        elif whettrrt[message.chat.id] == True:
             bot.send_message(message.chat.id, f"Не найден город, повторите ещё раз.")
     elif message.content_type == 'location':
-        if whether.whettrrt[message.chat.id] == True:
+        if whettrrt[message.chat.id] == True:
             latitude = message.location.latitude
             longitude = message.location.longitude
-            wet_geo = whether.Whether.get_whther_from_geo_data(latitude, longitude)
+            wet_geo = Whether.get_whther_from_geo_data(latitude, longitude)
             bot.send_message(message.chat.id, wet_geo)
-            whether.whettrrt[message.chat.id] = False
+            whettrrt[message.chat.id] = False
 
 bot.polling(none_stop=True, interval=0)
+
+
